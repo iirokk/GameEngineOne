8d94e5093bff5c54b1322b22b504064449c91511
Iiro Kokkonen <iiro.kokkonen@gmail.com>
2020-07-22 20:59:46 +0300

    Fixed skybox seams.

--- src/renderEngine/MasterRenderer.java
+++ src/renderEngine/MasterRenderer.java
@@ -21,7 +21,7 @@ public class MasterRenderer {
 
     private static final float FOV = 70;
     private static final float NEAR_PLANE = 0.1f;
-    private static final float FAR_PLANE = 1000;
+    private static final float FAR_PLANE = 1000;  // also modify sky box size accordingly
     private static final float SKY_RED = 0.42f;
     private static final float SKY_GREEN = 0.56f;
     private static final float SKY_BLUE = 0.65f;
